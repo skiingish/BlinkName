a44c0101a47c689a0263c3b2ad33794872ec24e4
skiingish <52271972+skiingish@users.noreply.github.com>
2020-03-21 17:42:54 +1100

    Add files via upload

--- blinkname.ino
+++ blinkname.ino
@@ -1,6 +1,8 @@
 int led1 = D7; //set digital pin 7 to var "led1"
 int dotTime = 200; // time for a dot
-int dashTime = 1000;// time for a dash
+int dashTime = 1000; // time for a dash
+int spaceTime = 1500; //time for a space
+int stopTime = 3000; // time for end of message before repeat
 
 void setup() {
     
@@ -9,9 +11,24 @@ void setup() {
 
 void loop() {
     
+    //S
+    dot(led1);
+    dot(led1);
+    dot(led1);
+    space();
+    //E
+    dot(led1);
+    space();
+    //A
     dot(led1);
     dash(led1);
+    space();
+    //N
+    dash(led1);
     dot(led1);
+    space();
+    // stop
+    stop(); 
     
 }
 
@@ -33,4 +50,16 @@ int dash(int LED) {
     delay(dashTime);
     digitalWrite(LED, LOW);
     delay(dotTime);
+}
+
+// function used for a space between letters
+
+int space() { 
+    delay(spaceTime); 
+}
+
+// function used to add time for a stop at the end of a message
+
+int stop() { 
+    delay(stopTime); 
 }
